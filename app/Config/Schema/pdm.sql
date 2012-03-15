ebbd1dadca25269a56cf5e39ee490b9e0f5b4209
unknown <akondratyev@.TIP.LOCAL>
2012-03-15 17:58:00 +0400

    more from work

--- app/Config/Schema/pdm.sql
+++ app/Config/Schema/pdm.sql
@@ -1,9 +1,9 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               5.1.54-1ubuntu4 - (Ubuntu)
--- Server OS:                    debian-linux-gnu
+-- Server version:               5.1.40-community - MySQL Community Server (GPL)
+-- Server OS:                    Win32
 -- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2012-02-20 23:17:12
+-- Date/time:                    2012-03-15 17:57:29
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -80,9 +80,9 @@ INSERT INTO `items_items` (`id`, `item_id`, `sub_item_id`) VALUES
 -- Dumping structure for table pdm.items_projects
 DROP TABLE IF EXISTS `items_projects`;
 CREATE TABLE IF NOT EXISTS `items_projects` (
-  `id` varchar(36) NOT NULL DEFAULT '',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` varchar(36) DEFAULT NULL,
-  `project_id` varchar(36) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
@@ -95,51 +95,48 @@ DELETE FROM `items_projects`;
 -- Dumping structure for table pdm.item_types
 DROP TABLE IF EXISTS `item_types`;
 CREATE TABLE IF NOT EXISTS `item_types` (
-  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(256) DEFAULT NULL,
   `suffix` varchar(256) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- Dumping data for table pdm.item_types: ~4 rows (approximately)
 DELETE FROM `item_types`;
 /*!40000 ALTER TABLE `item_types` DISABLE KEYS */;
 INSERT INTO `item_types` (`id`, `name`, `suffix`, `created`, `modified`) VALUES
-	(1, 'Assy', '000', '2012-01-26 10:32:48', '2012-02-20 23:12:26'),
-	(2, 'Part', '200', '2012-01-26 10:33:00', '2012-02-20 23:12:49'),
+	(1, 'Assy', '000', '2012-01-26 10:32:48', '2012-02-22 10:10:42'),
+	(2, 'Part', '200', '2012-01-26 10:33:00', '2012-02-22 10:06:58'),
 	(3, 'Std', 'std', '2012-01-26 10:33:20', '2012-02-20 23:13:20'),
-	(6, 'EQpart', '000', '2012-01-29 11:43:06', '2012-02-20 23:13:34');
+	(6, 'EQpart', 'eqp', '2012-01-29 11:43:06', '2012-02-22 09:46:12');
 /*!40000 ALTER TABLE `item_types` ENABLE KEYS */;
 
 
 -- Dumping structure for table pdm.projects
 DROP TABLE IF EXISTS `projects`;
 CREATE TABLE IF NOT EXISTS `projects` (
-  `id` varchar(36) NOT NULL DEFAULT '',
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `projectname` varchar(256) DEFAULT NULL,
   `description` text,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
--- Dumping data for table pdm.projects: ~4 rows (approximately)
+-- Dumping data for table pdm.projects: ~1 rows (approximately)
 DELETE FROM `projects`;
 /*!40000 ALTER TABLE `projects` DISABLE KEYS */;
 INSERT INTO `projects` (`id`, `projectname`, `description`, `created`, `modified`) VALUES
-	('4f0c0427-8918-46fe-9f1a-0b089be3e0a3', 'ss-113', 'sdf sadf sadf', '2012-01-10 09:25:59', '2012-01-10 11:10:07'),
-	('4f0c1654-a998-4068-ad63-0b089be3e0a3', 'TT-909', 'New description ', '2012-01-10 10:43:32', '2012-01-11 11:15:36'),
-	('4f0c1c04-e580-45b7-a5b2-0b089be3e0a3', 'TT-118', 'test is just. hello 33', '2012-01-10 11:07:48', '2012-01-11 11:14:28'),
-	('4f0d6f8a-2218-4903-9e09-0b089be3e0a3', 'TT-222', 'Just a tt 222', '2012-01-11 11:16:26', '2012-01-11 11:16:26');
+	(1, 'ks-11', 'descr', '2012-03-11 13:30:14', '2012-03-11 13:30:14');
 /*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 
 
 -- Dumping structure for table pdm.project_plans
 DROP TABLE IF EXISTS `project_plans`;
 CREATE TABLE IF NOT EXISTS `project_plans` (
-  `id` varchar(36) NOT NULL DEFAULT '',
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `project_id` varchar(256) DEFAULT NULL,
   `name` varchar(256) DEFAULT NULL,
   `location` text,
@@ -148,14 +145,9 @@ CREATE TABLE IF NOT EXISTS `project_plans` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
--- Dumping data for table pdm.project_plans: ~4 rows (approximately)
+-- Dumping data for table pdm.project_plans: ~0 rows (approximately)
 DELETE FROM `project_plans`;
 /*!40000 ALTER TABLE `project_plans` DISABLE KEYS */;
-INSERT INTO `project_plans` (`id`, `project_id`, `name`, `location`, `created`, `modified`) VALUES
-	('4f0c326b-fe98-459e-89a0-0b089be3e0a3', '4f0c0427-8918-46fe-9f1a-0b089be3e0a3', 'PP-Name01', 'get it 33', '2012-01-10 12:43:23', '2012-01-11 11:13:50'),
-	('4f0c33f0-5be4-47e9-8cb8-0b089be3e0a3', '4f0c0427-8918-46fe-9f1a-0b089be3e0a3', 'prom ', 'brom', '2012-01-10 12:49:52', '2012-01-10 13:54:00'),
-	('4f0c38d0-424c-442c-bb17-0b089be3e0a3', '4f0c1c04-e580-45b7-a5b2-0b089be3e0a3', 'what', 'novij povorot', '2012-01-10 13:10:40', '2012-01-10 13:54:10'),
-	('4f27e117-b674-476e-9d91-0ca89be3e0a3', '4f0c0427-8918-46fe-9f1a-0b089be3e0a3', 'Hey', 'To del', '2012-01-31 12:39:51', '2012-01-31 12:39:51');
 /*!40000 ALTER TABLE `project_plans` ENABLE KEYS */;
 
 
@@ -174,41 +166,34 @@ CREATE TABLE IF NOT EXISTS `trays` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=65 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=74 DEFAULT CHARSET=utf8;
 
--- Dumping data for table pdm.trays: 29 rows
+-- Dumping data for table pdm.trays: 22 rows
 DELETE FROM `trays`;
 /*!40000 ALTER TABLE `trays` DISABLE KEYS */;
 INSERT INTO `trays` (`id`, `parent_id`, `lft`, `rght`, `name`, `item_type_id`, `project_id`, `ata_code`, `ata_cache`, `created`, `modified`) VALUES
-	(1, NULL, 1, 58, 'Root Trd', 0, NULL, NULL, NULL, NULL, NULL),
-	(2, 1, 2, 53, 'Rifles', 1, NULL, 'R', NULL, NULL, '2012-01-31 06:04:30'),
-	(3, 2, 3, 16, 'Barrels', 1, NULL, '1', NULL, NULL, '2012-01-30 13:53:41'),
-	(5, 3, 6, 15, 'Muzzle breaks', 1, NULL, '4', NULL, NULL, '2012-01-31 15:46:11'),
-	(6, 2, 17, 48, 'Actions', 1, NULL, NULL, NULL, NULL, NULL),
-	(7, 6, 18, 27, 'Resivers', 1, NULL, '1', NULL, NULL, '2012-01-31 15:49:42'),
-	(8, 6, 28, 47, 'Bolts', 1, NULL, '', NULL, NULL, '2012-01-29 22:31:35'),
-	(9, 1, 54, 57, 'Tools', 0, NULL, NULL, NULL, NULL, NULL),
-	(10, 9, 55, 56, 'Crabs', 0, NULL, NULL, NULL, NULL, NULL),
-	(28, 2, 49, 50, 'Stocks', 1, NULL, '', NULL, NULL, '2012-01-29 21:47:54'),
-	(29, 8, 29, 30, 'Bolt', 2, NULL, NULL, NULL, NULL, NULL),
-	(30, 8, 31, 32, 'Bolt sleeve', 2, NULL, NULL, NULL, NULL, NULL),
-	(32, 8, 33, 34, 'Bolt clamp', 2, NULL, '', NULL, NULL, '2012-01-29 22:33:17'),
-	(27, 3, 4, 5, 'Barrels', 2, NULL, '3', NULL, NULL, '2012-01-31 15:45:09'),
-	(36, 7, 19, 20, 'Handle limit', 2, NULL, NULL, NULL, NULL, NULL),
-	(34, 8, 37, 38, 'Bolt stop', 2, NULL, '', NULL, NULL, '2012-01-29 22:33:07'),
-	(33, 8, 35, 36, 'Firing Pin', 2, NULL, '', NULL, NULL, '2012-01-29 22:32:59'),
-	(39, 7, 21, 22, 'Thread insert', 2, NULL, NULL, NULL, NULL, NULL),
-	(38, 5, 9, 10, 'Nut', 2, NULL, '2', NULL, NULL, '2012-01-30 13:55:21'),
-	(37, 5, 7, 8, 'Muzzle break', 2, NULL, '3', NULL, NULL, '2012-01-31 15:46:21'),
-	(40, 7, 23, 24, 'Resivers', 2, NULL, NULL, NULL, NULL, NULL),
-	(44, 2, 51, 52, 'biPods', 3, NULL, '', NULL, NULL, '2012-01-29 22:34:38'),
-	(58, 8, 39, 44, 'to del', 1, NULL, NULL, '', '2012-01-31 15:31:15', '2012-01-31 15:31:15'),
-	(62, 5, 11, 14, 'Just ro tel', 1, NULL, '8', '148', '2012-01-31 15:48:00', '2012-01-31 15:48:00'),
-	(59, 8, 45, 46, 'to del', 1, NULL, NULL, '', '2012-01-31 15:31:43', '2012-01-31 15:31:43'),
-	(60, 58, 40, 43, 'hz1', 1, NULL, NULL, '', '2012-01-31 15:32:04', '2012-01-31 15:32:04'),
-	(61, 60, 41, 42, 'hz2', 1, NULL, NULL, '', '2012-01-31 15:32:15', '2012-01-31 15:32:15'),
-	(63, 62, 12, 13, 'More just rotel', 2, NULL, NULL, '148', '2012-01-31 15:48:29', '2012-01-31 15:48:29'),
-	(64, 7, 25, 26, 'fsdg 234', 2, NULL, '9', '19', '2012-01-31 15:50:03', '2012-01-31 15:50:03');
+	(1, NULL, 1, 44, 'Root Tray', 1, NULL, '', 'xxx', NULL, NULL),
+	(2, 1, 2, 39, 'Rifles', 1, NULL, 'R', '000', NULL, '2012-01-31 06:04:30'),
+	(3, 2, 3, 12, 'Barrels', 1, NULL, '1', '100', NULL, '2012-02-26 09:12:42'),
+	(5, 3, 6, 11, 'MuzzleBreaks', 1, NULL, '6', '160', NULL, '2012-02-22 12:56:00'),
+	(6, 2, 13, 34, 'Actions', 1, NULL, NULL, NULL, NULL, NULL),
+	(7, 6, 14, 21, 'Resivers', 1, NULL, '1', NULL, NULL, '2012-01-31 15:49:42'),
+	(8, 6, 22, 33, 'Bolts', 1, NULL, '', NULL, NULL, '2012-01-29 22:31:35'),
+	(9, 1, 40, 43, 'Tools', 1, NULL, NULL, '000', NULL, NULL),
+	(10, 9, 41, 42, 'Crabs', 0, NULL, NULL, NULL, NULL, NULL),
+	(28, 2, 35, 36, 'Stocks', 1, NULL, '', NULL, NULL, '2012-01-29 21:47:54'),
+	(29, 8, 23, 24, 'Bolt', 2, NULL, NULL, NULL, NULL, NULL),
+	(30, 8, 25, 26, 'Bolt sleeve', 2, NULL, NULL, NULL, NULL, NULL),
+	(32, 8, 27, 28, 'Bolt clamp', 2, NULL, '', NULL, NULL, '2012-01-29 22:33:17'),
+	(27, 3, 4, 5, 'Barrel', 2, NULL, '5', '150', NULL, '2012-02-22 11:41:17'),
+	(36, 7, 15, 16, 'Handle limit', 2, NULL, NULL, NULL, NULL, NULL),
+	(34, 8, 31, 32, 'Bolt stop', 2, NULL, '', NULL, NULL, '2012-01-29 22:33:07'),
+	(33, 8, 29, 30, 'Firing Pin', 2, NULL, '', NULL, NULL, '2012-01-29 22:32:59'),
+	(39, 7, 17, 18, 'Thread insert', 2, NULL, NULL, NULL, NULL, NULL),
+	(38, 5, 9, 10, 'Nut', 2, NULL, '2', '142', NULL, '2012-02-22 11:43:22'),
+	(37, 5, 7, 8, 'Muzzle break', 2, NULL, '3', '143', NULL, '2012-02-22 11:41:47'),
+	(40, 7, 19, 20, 'Resivers', 2, NULL, NULL, NULL, NULL, NULL),
+	(44, 2, 37, 38, 'biPods', 1, NULL, '', NULL, NULL, '2012-01-29 22:34:38');
 /*!40000 ALTER TABLE `trays` ENABLE KEYS */;
 /*!40014 SET FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- app/Model/Project.php
+++ app/Model/Project.php
@@ -1,10 +1,13 @@
 <?php
+
 App::uses('AppModel', 'Model');
+
 /**
  * Project Model
  *
  */
 class Project extends AppModel {
+
     public $name = 'Project';
     public $hasMany = array(
         'ProjectPlan' => array(
@@ -14,6 +17,27 @@ class Project extends AppModel {
         'Tray' => array(
             'className' => 'Tray',
             'foreignKey' => 'project_id'
-        )        
-        );    
+        )
+    );
+
+    public $hasAndBelongsToMany = array(
+        'Item' =>
+            array(
+                'className'              => 'Item',
+                'joinTable'              => 'items_projects',
+                'foreignKey'             => 'poject_id',
+                'associationForeignKey'  => 'item_id',
+                'unique'                 => true,
+                'conditions'             => '',
+                'fields'                 => '',
+                'order'                  => '',
+                'limit'                  => '',
+                'offset'                 => '',
+                'finderQuery'            => '',
+                'deleteQuery'            => '',
+                'insertQuery'            => ''
+            )
+    );    
+    
 }
+?>
